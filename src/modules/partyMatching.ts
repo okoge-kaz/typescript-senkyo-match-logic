73c0a9c85d06f5c76b1b639712422ee4446adb29
Fujii Kazuki <kaz.tokyo.tech20@gmail.com>
2022-05-05 23:22:38 +0900

    update api logic

--- src/modules/partyMatching.ts
+++ src/modules/partyMatching.ts
@@ -1,8 +1,62 @@
-import { PartyAnswerForEachQuestion, UserAnswerForEachQuestion } from "../class/answer";
+import {
+	PartyAnswerForEachQuestion,
+	UserAnswerForEachQuestion,
+} from "../class/answer";
+import { MatchingScoreByCategory } from "../class/matching";
 import calculateMatchingScore from "./calculateMatchingScore";
 
-const calculatePartyMatchingScore = (userSelectThemes: string[], userAnswerDataList: UserAnswerForEachQuestion[]) => {
-  const partyList: string[] = [
+interface PartyAnswer {
+	question_id: number;
+	category: string;
+	question: string;
+	party_answer: string;
+	party_answer_value: number;
+	answer_value_size: number;
+}
+
+interface MatchingResultResponse {
+	totalMatchingScore: number;
+	matchingScoreByCategory: MatchingScoreByCategory;
+}
+
+interface PartyMatchingResult {
+	partyMatchScoreResult: Map<string, number>;
+	partyThemeBasedMatchScoreResult: Map<string, Map<string, number>>;
+}
+
+const convertToPartyAnswerForEachQuestion = (
+	questionId: number,
+	category: string,
+	questionContent: string,
+	partyAnswer: string,
+	partyAnswerValue: number,
+	answerValueSize: number
+) => {
+	return new PartyAnswerForEachQuestion(
+		questionId,
+		category,
+		questionContent,
+		partyAnswer,
+		partyAnswerValue,
+		answerValueSize
+	);
+};
+
+const getPartyAnswerData = async (party: string) => {
+	const response = await fetch(
+		"https://github.com/okoge-kaz/typescript-senkyo-match-logic/blob/main/data/advance/party/" +
+			party +
+			".json"
+	);
+	const partyAnswerDataList: PartyAnswer[] = await response.json();
+	return partyAnswerDataList;
+};
+
+export const calculatePartyMatchingScore = (
+	userSelectThemes: string[],
+	userAnswerDataList: UserAnswerForEachQuestion[]
+): PartyMatchingResult => {
+	const partyList: string[] = [
 		"ishin",
 		"kokumin",
 		"komei",
@@ -11,12 +65,53 @@ const calculatePartyMatchingScore = (userSelectThemes: string[], userAnswerDataL
 		"nhk",
 		"ritumin",
 		"syamin",
-  ];
-  
-  const partyMatchScoreResult: Object = {};
-  const partyThemeBasedMatchScoreResult: Object = {};
+	];
 
-  partyList.forEach((party) => { 
-    
-  })
-}
+	const partyMatchScoreResult: Map<string, number> = new Map();
+	const partyThemeBasedMatchScoreResult: Map<
+		string,
+		Map<string, number>
+	> = new Map();
+
+	partyList.forEach((party) => {
+		const rawPartyAnswerDataList = getPartyAnswerData(party);
+		rawPartyAnswerDataList.then((rawPartyAnswerDataList) => {
+			const partyAnswerDataList: PartyAnswerForEachQuestion[] =
+				rawPartyAnswerDataList.map((partyAnswerData) =>
+					convertToPartyAnswerForEachQuestion(
+						partyAnswerData.question_id,
+						partyAnswerData.category,
+						partyAnswerData.question,
+						partyAnswerData.party_answer,
+						partyAnswerData.party_answer_value,
+						partyAnswerData.answer_value_size
+					)
+				);
+
+			const matchingResult: MatchingResultResponse = calculateMatchingScore(
+				userSelectThemes,
+				userAnswerDataList,
+				partyAnswerDataList,
+				"party"
+			);
+
+			const totalMatchingScore: number = matchingResult.totalMatchingScore;
+			const matchingScoreByCategory: MatchingScoreByCategory =
+				matchingResult.matchingScoreByCategory;
+
+			partyMatchScoreResult.set(party, Math.round(totalMatchingScore));
+			partyThemeBasedMatchScoreResult.set(
+				party,
+				matchingScoreByCategory.getMatchingScoreByCategoryDictionary()
+			);
+		});
+	});
+
+	return {
+		// value の昇順sort
+		partyMatchScoreResult: new Map(
+			[...partyMatchScoreResult].sort((key, value) => key[1] - value[1])
+		),
+		partyThemeBasedMatchScoreResult: partyThemeBasedMatchScoreResult,
+	};
+};
---
+++ src/modules/politicianMatching.ts
@@ -0,0 +1,108 @@
+import {
+	PoliticianAnswerForEachQuestion,
+	UserAnswerForEachQuestion,
+} from "../class/answer";
+import { MatchingScoreByCategory } from "../class/matching";
+import calculateMatchingScore from "./calculateMatchingScore";
+
+interface PoliticianAnswer {
+	question_id: number;
+	category: string;
+	question: string;
+	politician_answer: string;
+	politician_answer_value: number;
+	answer_value_size: number;
+}
+
+interface MatchingResultResponse {
+	totalMatchingScore: number;
+	matchingScoreByCategory: MatchingScoreByCategory;
+}
+
+interface PoliticianMatchingResult {
+	politicianMatchScoreResult: Map<string, number>;
+	politicianThemeBasedMatchScoreResult: Map<string, Map<string, number>>;
+}
+
+const convertToPoliticianAnswerForEachQuestion = (
+	questionId: number,
+	category: string,
+	questionContent: string,
+	politicianAnswer: string,
+	politicianAnswerValue: number,
+	answerValueSize: number
+) => {
+	return new PoliticianAnswerForEachQuestion(
+		questionId,
+		category,
+		questionContent,
+		politicianAnswer,
+		politicianAnswerValue,
+		answerValueSize
+	);
+};
+
+const getPoliticianAnswerData = async (politician: string) => {
+	const response = await fetch(
+		"https://github.com/okoge-kaz/typescript-senkyo-match-logic/blob/main/data/advance/politician/" +
+			politician +
+			".json"
+	);
+	const politicianAnswerDataList: PoliticianAnswer[] = await response.json();
+	return politicianAnswerDataList;
+};
+
+export const calculatePoliticianMatchingScore = (
+	userSelectThemes: string[],
+	userAnswerDataList: UserAnswerForEachQuestion[]
+): PoliticianMatchingResult => {
+	const politicianList: string[] = ["sample"];
+
+	const politicianMatchScoreResult: Map<string, number> = new Map();
+	const politicianThemeBasedMatchScoreResult: Map<
+		string,
+		Map<string, number>
+	> = new Map();
+
+	politicianList.forEach((politician) => {
+		const rawPoliticianAnswerDataList = getPoliticianAnswerData(politician);
+		rawPoliticianAnswerDataList.then((rawPoliticianAnswerDataList) => {
+			const politicianAnswerDataList: PoliticianAnswerForEachQuestion[] =
+				rawPoliticianAnswerDataList.map((politicianAnswerData) =>
+					convertToPoliticianAnswerForEachQuestion(
+						politicianAnswerData.question_id,
+						politicianAnswerData.category,
+						politicianAnswerData.question,
+						politicianAnswerData.politician_answer,
+						politicianAnswerData.politician_answer_value,
+						politicianAnswerData.answer_value_size
+					)
+				);
+
+			const matchingResult: MatchingResultResponse = calculateMatchingScore(
+				userSelectThemes,
+				userAnswerDataList,
+				politicianAnswerDataList,
+				"politician"
+			);
+
+			const totalMatchingScore: number = matchingResult.totalMatchingScore;
+			const matchingScoreByCategory: MatchingScoreByCategory =
+				matchingResult.matchingScoreByCategory;
+
+			politicianMatchScoreResult.set(politician, Math.round(totalMatchingScore));
+			politicianThemeBasedMatchScoreResult.set(
+				politician,
+				matchingScoreByCategory.getMatchingScoreByCategoryDictionary()
+			);
+		});
+	});
+
+	return {
+		// value の昇順sort
+		politicianMatchScoreResult: new Map(
+			[...politicianMatchScoreResult].sort((key, value) => key[1] - value[1])
+		),
+		politicianThemeBasedMatchScoreResult: politicianThemeBasedMatchScoreResult,
+	};
+};
