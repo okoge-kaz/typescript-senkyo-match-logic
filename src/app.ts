ef2332fdb33d1661ec01b6653498801e8d38b4b1
Fujii Kazuki <kaz.tokyo.tech20@gmail.com>
2022-05-05 23:23:38 +0900

    update api main code

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express, { Application, Request, Response } from "express";
+import { UserAnswerForEachQuestion } from "./class/answer";
+import { calculatePartyMatchingScore } from "./modules/partyMatching";
+import { calculatePoliticianMatchingScore } from "./modules/politicianMatching";
 
 const app: Application = express();
 const PORT: number = 3000;
@@ -12,21 +15,93 @@ app.get("/", async (_req: Request, res: Response) => {
 	});
 });
 
-app.get("/api/party/matching", async (_req: Request, res: Response) => {});
+app.get("/api/party/matching", async (_req: Request, res: Response) => {
 
-app.get(
-	"/api/party/theme_matching",
-	async (_req: Request, res: Response) => {}
-);
+	const userSelectThemes: string[] = _req.body.themes;
+	const userAnswerDataList: UserAnswerForEachQuestion[] =
+		_req.body.answers.json();
 
-app.get("/api/politician/matching", async (_req: Request, res: Response) => {});
+	const partyMatchingResult = calculatePartyMatchingScore(
+		userSelectThemes,
+		userAnswerDataList
+	);
+
+	return res
+		.status(200)
+		.send({ partyTotalMatchScore: partyMatchingResult.partyMatchScoreResult });
+});
+
+app.get("/api/party/theme_matching", async (_req: Request, res: Response) => {
+	const userSelectThemes: string[] = _req.body.themes;
+	const userAnswerDataList: UserAnswerForEachQuestion[] = _req.body.answers.json();
+
+	const partyMatchingResult = calculatePartyMatchingScore(
+		userSelectThemes,
+		userAnswerDataList
+	);
+
+	return res.status(200).send({
+		partyThemeBasedMatchScore:
+			partyMatchingResult.partyThemeBasedMatchScoreResult,
+	});
+});
+
+app.get("/api/politician/matching", async (_req: Request, res: Response) => {
+	const userSelectThemes: string[] = _req.body.themes;
+	const userAnswerDataList: UserAnswerForEachQuestion[] =
+		_req.body.answers.json();
+
+	const politicianMatchingResult = calculatePoliticianMatchingScore(
+		userSelectThemes,
+		userAnswerDataList
+	);
+
+	return res.status(200).send({
+		politicianTotalMatchScore:
+			politicianMatchingResult.politicianMatchScoreResult,
+	});
+});
 
 app.get(
 	"/api/politician/theme_matching",
 	async (_req: Request, res: Response) => {
-		
+		const userSelectThemes: string[] = _req.body.themes;
+		const userAnswerDataList: UserAnswerForEachQuestion[] =
+			_req.body.answers.json();
+
+		const politicianMatchingResult = calculatePoliticianMatchingScore(
+			userSelectThemes,
+			userAnswerDataList
+		);
+
+		return res.status(200).send({
+			politicianThemeBasedMatchScore:
+				politicianMatchingResult.politicianThemeBasedMatchScoreResult,
+		});
 	}
 );
+
+const partyList: string[] = [
+	"ishin",
+	"kokumin",
+	"komei",
+	"kyosan",
+	"ldp",
+	"nhk",
+	"ritumin",
+	"syamin",
+];
+
+partyList.forEach((party) => {
+	app.get(`/api/data/party/${party}/`, async (_req: Request, res: Response) => {
+		const response = await fetch(
+			"https://github.com/okoge-kaz/typescript-senkyo-match-logic/tree/main/data/advance/party/" +
+				party +
+				".json"
+		);
+		return res.status(200).send(await response.json());
+	});
+});
 
 try {
 	app.listen(PORT, () => {
